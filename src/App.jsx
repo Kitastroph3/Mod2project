7c69bb3576163d21027c2660d1dc584f5549a0f4
Kit Danovsky <cadanovsky@gmail.com>
2023-12-04 11:45:09 -0500

    progress

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,8 @@ const hexInput = "hex="+"0047AB"
 // https://www.thecolorapi.com/docs
 // https://www.thecolorapi.com/scheme?hex=0047AB&rgb=0,71,171&hsl=215,100%,34%&cmyk=100,58,0,33&format=json&mode=analogic&count=6
 // const URL = ` https://www.thecolorapi.com/scheme?${input}&format=json&mode=${mode}&count=${count}`
-const URL= `https://www.thecolorapi.com/scheme?${hexInput}&rgb=0,71,171&hsl=215,100%,34%&cmyk=100,58,0,33&format=json&mode=analogic&count=6
-`
+const URL = `https://www.thecolorapi.com/scheme?${hexInput}&rgb=0,71,171&hsl=215,100%,34%&cmyk=100,58,0,33&format=json&mode=analogic&count=6`
+
 function App() {
   const [hexInput, setHexInput] = useState(0)
   // const [NJhumidity, NJsetHumidity] = useState(0)
@@ -27,28 +27,19 @@ function App() {
     fetchData();
   }, []);
 
-  ///
-  
-  // const [MAtemp, MAsetTemp] = useState(0)
-  // const [MAhumidity, MAsetHumidity] = useState(0)
-
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const result = await fetch(MAURL);
-  //     result.json().then(json => {  
-  //       MAsetTemp(json.current.temp_f)
-  //       MAsetHumidity(json.current.humidity)
-  //     })
-  //   }
-  //   fetchData();
-  // }, []);
-
   return (
     <div className='App'>
       <header><h1>Color</h1></header>
-      <input type="text" id="hexText"></input>
-      
-      <footer>API Practice- Kit Danovsky</footer>
+      <div id="hex">
+         <input type="text" placeholder="Enter Hex Value Here" id="hexText"></input><button>Submit</button>
+      </div>
+      <div id="Return">
+        <div id="one">Example</div>
+        <div id="two">Example</div>
+        <div id="three">Example</div>
+        <div id="four">Example</div>
+      </div>
+      <footer>Mod2 Project- Kit Danovsky</footer>
     </div>
   )
 }
