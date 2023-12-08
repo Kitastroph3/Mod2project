fd5570977e2b3e262e04888e81808a5d84e6783b
Kit Danovsky <cadanovsky@gmail.com>
2023-12-08 11:08:26 -0500

    Routes Router and Links added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import "./App.css";
-// import CreateUser from './components/CreateUser';
-// import Colors from './components/Colors';
-// import Users from './pages/Users';
+import { Route, Routes } from 'react-router-dom';
 import TopNav from './components/TopNav';
-
+import About from "./pages/About";
+import Users from './pages/Users';
+import ColorScheme from "./pages/ColorScheme";
+  
 function App () {
   return (
     <div id="App">
       <TopNav />
-      {/* <div>UI/UX Helper</div> */}
-      <div id="container">
-        {/* <Colors /> */}
+      <div id="mainContainer">
+        <Routes>
+          <Route path="/" element={<About />} />
+          <Route path="/users" element={<Users/>}/>
+          <Route path="/colors" element={<ColorScheme/>}/>
+        </Routes>
       </div>
     </div>
   )
--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function TopNav () {
   return (
     <nav>
       <div id="navcontainer">
-        <div className='page'>About</div>
-        <div className='page'>Users</div>
-        <div className='page'>Colors</div>
+        <Link to ="/">
+          <div className='page'>About</div>
+        </Link>
+
+        <Link to ="/users">
+          <div className='page'>Users</div>
+        </Link>
+
+        <Link to ="/colors">
+          <div className='page'>Colors</div>
+        </Link>
       </div>
     </nav>
   )
