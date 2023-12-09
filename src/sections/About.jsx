ea901fb678f7df7d94df5abc2e07c39bb6d8994c
Kit Danovsky <cadanovsky@gmail.com>
2023-12-09 14:07:29 -0500

    lunch break

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -2,12 +2,8 @@ import React from 'react'
 
 const About = () => {
   return (
-      <section id="About">
-          <div>About</div>
-          <div id="Welcome">
-            <div>Welcome to Webspiration, a reference for user personas and design schemes</div>
-            <div>Documentation and technologies used</div>
-          </div>
+      <section id="Home">
+        <div id="intro"> </div>
       </section>
   )
 }
