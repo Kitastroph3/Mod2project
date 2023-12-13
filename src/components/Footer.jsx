a72f2a0f981ae07bb7b3d0c178365efc0e74b1e7
Kit Danovsky <cadanovsky@gmail.com>
2023-12-13 14:12:30 -0500

    added icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,14 @@ const Footer = () => {
   return (
     <footer>
       <>designed by Kit</>
+      <div className='icons'>
+      <a href='https://github.com/Kitastroph3/'>
+        <img className="icon" id="icon1" src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn0.iconfinder.com%2Fdata%2Ficons%2Fshift-logotypes%2F32%2FGithub-512.png&f=1&nofb=1&ipt=819e0ea804501a64d24a2def9f9530d93c80345c357d6648e8cd63d18dc84bce&ipo=images' />
+      </a>
+      <a href="https://www.linkedin.com/in/kit-danovsky-235278165?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BDyWef67ARNi4MRdaByYVXg%3D%3D">
+        <img className='icon' src="http://shevlinatkins.com/wp-content/uploads/2018/03/linkedin-logo.png"/>
+        </a>
+      </div>
     </footer>
   )
 }
